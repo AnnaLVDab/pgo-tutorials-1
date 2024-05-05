68983878724bd98667f728aa0ab1f7d82dc8f995
AnnaLVDab <155486039+AnnaLVDab@users.noreply.github.com>
2024-05-05 15:09:58 +0200

    Add files via upload

---
+++ Main copy.java
@@ -0,0 +1,25 @@
+import java.math.BigDecimal;
+
+public class Main {
+    public static void main(String[] args) {
+        // pusty main
+    }
+}
+
+class Multiplier {
+    public int multiply(int x, int y){
+        return x * y;
+    }
+
+    public int multiply(int x, int y, int z, int q){
+        return x * y * z * q;
+    }
+
+    public double multiply(double a, double b){
+        return a * b;
+    }
+
+  // BigDecimal - dwa obiekty
+
+
+}
